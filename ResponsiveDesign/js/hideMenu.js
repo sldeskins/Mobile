e36464a56ea2a4bf4c86e33941393d79d12e25d7
sldeskins <sldeskins@gmail.com>
2015-10-28 15:12:59 -0400

    03 navigation using JQuery show

---
+++ ResponsiveDesign/js/hideMenu.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $(function () {
+        $('#showPhoneNav').click(function () {
+            $("#myNav").slideToggle('normal', function () {
+                if ($('#myNav').is(':visible')) {
+                    $('#showPhoneNav').text('Hide Navigation');
+                } else {
+                    $('#showPhoneNav').text('Show Navigation');
+                }
+            });
+        });
+    });
+});
